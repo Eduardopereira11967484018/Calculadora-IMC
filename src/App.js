4259313e64eff923460c6621663d7dda37c20d1e
Eduardopereira11967484018 <eduardoperira.lima@yahoo.com.br>
2024-06-29 14:43:48 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [altura, setAltura] = useState('');
+  const [peso, setPeso] = useState('');
+  const [imc, setImc] = useState('');
+  const [classificacao, setClassificacao] = useState('');
+
+  const calcularIMC = (e) => {
+    e.preventDefault();
+    const alturaEmMetros = altura / 100;
+    const imcCalculado = peso / (alturaEmMetros * alturaEmMetros);
+    setImc(imcCalculado.toFixed(2));
+
+    if (imcCalculado < 18.5) {
+      setClassificacao('Abaixo do peso');
+    } else if (imcCalculado >= 18.5 && imcCalculado <= 24.9) {
+      setClassificacao('Peso normal');
+    } else if (imcCalculado >= 25 && imcCalculado <= 29.9) {
+      setClassificacao('Sobrepeso');
+    } else {
+      setClassificacao('Obesidade');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Calcular IMC</h1>
+      <form onSubmit={calcularIMC}>
+        <div>
+          <div>
+            <label>
+              Altura (cm):
+              <input
+                type="number"
+                value={altura}
+                onChange={(e) => setAltura(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Peso (kg):
+              <input
+                type="number"
+                value={peso}
+                onChange={(e) => setPeso(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          <button type="submit">Calcular IMC</button>
+        </div>
+      </form>
+      <p>Seu IMC é: {imc}</p>
+      <p>Classificação: {classificacao}</p>
     </div>
   );
 }
